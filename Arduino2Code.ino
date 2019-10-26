40360cb04e56bb33695016f26243a26b815c6c92
Rishi Singhal <13864395+rs872@users.noreply.github.com>
2019-10-26 17:19:32 -0400

    Update Arduino2Code.ino

--- Arduino2Code.ino
+++ Arduino2Code.ino
@@ -13,10 +13,6 @@ char buff [50];
 volatile byte indx;
 volatile boolean process;
 int  interruptPin = 10;
-int motorPin1 = 2;
-int motorPin2 = 3;
-int motorPin3 = 8;
-int motorPin4 = 5;
 //LEDs for RFID
 int LED1 = 14;    //Green LED
 int LED2 = 6;    //Yellow LED
@@ -31,11 +27,7 @@ void setup() {
   myservo.attach(6);//servo pin
   pinMode(MISO,OUTPUT); //init spi
   pinMode(3,OUTPUT);
-  //Init DC pins
-  pinMode(motorPin1, OUTPUT);
-  pinMode(motorPin2, OUTPUT);
-  pinMode(motorPin3, OUTPUT);
-  pinMode(motorPin4, OUTPUT);
+
   pinMode(20,OUTPUT);
   pinMode(22,OUTPUT);
   
@@ -69,8 +61,7 @@ void setup() {
   
   pinMode(interruptPin,INPUT);
   int val = digitalRead(interruptPin);
-  //if (!val)
-    //Serial.println ("wtf");
+
   delay(1000);
   SPI.attachInterrupt();
  // attachInterrupt(digitalPinToInterrupt(interruptPin), tester, LOW); //enable interrupt
@@ -96,11 +87,7 @@ void moveForward() {
 }
 
 void loop() {
-//
-          digitalWrite(motorPin1, LOW);
-          digitalWrite(motorPin2, LOW);
-          digitalWrite(motorPin3, LOW);
-          digitalWrite(motorPin4, LOW);  
+
 
   if (process) {
     buff[indx] = 0;
@@ -110,41 +97,12 @@ void loop() {
     Serial.print(buff); //print to serial monitor
     int i = 0;
     if (i < sizeof(buff)) {
-      switch(buff[i]) {
-        case 'F' : //fwd
-          moveForward();
-          delay(6000);
-          break;
-        case 'B' : //Backwards (back())
-          digitalWrite(motorPin1, LOW);
-          digitalWrite(motorPin2, HIGH);
-          digitalWrite(motorPin3, LOW);
-          digitalWrite(motorPin4, HIGH);
-          delay(6000);
-          break;
-        case 'L' : //left
-          digitalWrite(motorPin1, HIGH);
-          digitalWrite(motorPin2, LOW);
-          digitalWrite(motorPin3, LOW);
-          digitalWrite(motorPin4, HIGH);
-          delay(6000);
-          break;
-        case 'R' : //right
-          digitalWrite(motorPin1, LOW);
-          digitalWrite(motorPin2, HIGH);
-          digitalWrite(motorPin3, HIGH);
-          digitalWrite(motorPin4, LOW);
-          delay(6000);
-          break; 
-          
+      switch(buff[i]) {         
         case 'O': //object detection
-          RFID();
-         
-         
+          RFID();         
         default:
           i++;
           break; 
-        
       }
     }
   }
